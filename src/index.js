38786cc14f581872b2791668f03abf31d84d0724
Shashank <unique.shashank02@gmail.com>
2025-10-26 13:08:24 +0530

    Error and async handling file update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import connect_db from './db/connect_db.js';
 import dotenv from 'dotenv';
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env',
 });
 
-connect_db();
+connect_db()
+.then(() => {
+    app.on("error", (err) => {
+        console.log("Express app error", err);
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connectiln failes: ", err);
+});
