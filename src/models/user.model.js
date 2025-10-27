b476f078610240244a99eb2dd79d4dccdba9c3ba
Shashank <unique.shashank02@gmail.com>
2025-10-27 09:57:42 +0530

    file uploading middleware done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -70,10 +70,7 @@ userSchema.methods.generateAccessToken = function(){
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id: this._id,
-            username: this.username,
-            email: this.email,
-            fullName: this.fullName,
+            _id: this._id
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
